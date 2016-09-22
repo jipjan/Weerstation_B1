f3c20cdeb6856816c6f0d5dc458c0e0805a75cf9
Robin Hobbel <rm.hobbel@telfort.nl>
2016-09-22 13:47:00 +0200

    Finishing touch, Complete version week 4.

--- awstest/Measurement.java
+++ awstest/Measurement.java
@@ -3,141 +3,182 @@
  */
 public class Measurement
 {
-   private WeatherStation W;
    private MetingenHandler M;
-   private double Barometer;
-   private double InsideTemp;
-   private double OutsideTemp;
-   private double InsideHum;
-   private double OutsideHum;
-   private double WindSpeed;
-   private String WindDirection;
-   private double AvgWindSpeed;
-   private double RainRate;
-   private double UVindex;
-   private double Battery;
-   private String SunRise;
-   private String SunSet;
-   private double HeatIndex;
-   private double Dewpoint;
-   private double SolarRad;
-   private double WindChill;
+   private RawMeasurement rawMeasurement;
    
-   public Measurement()
+   public Measurement(RawMeasurement raw)
    {
-       W = new WeatherStation();
        M = new MetingenHandler();
+       rawMeasurement = raw;
    }
    
-   /**
-    * This one is not needed for the assignment.
-    */
-   public Measurement(String host, String port, String dbName, String userName, String password)
+   public java.sql.Timestamp Time()
    {
-       W = new WeatherStation( host,  port,  dbName,  userName,  password);
-       M = new MetingenHandler();
+       return rawMeasurement.getDateStamp();
    }
-   
-   public void setWeatherStation(WeatherStation WS)
-   {
-       W = WS;
-   }
-   
-   public WeatherStation getWeatherStation()
-   {
-       return W;
-   }
-
    public double BarometerData()
    {
-       return M.luchtdruk(W.getMostRecentBarometer());
+       return M.luchtdruk(rawMeasurement.getBarometer());
    }
-   
-   public void SetBarometerData()
-   {
-       
-   }
-   
+  
    public double TemperatuurInsideData()
    {
-       return M.temperatuur(W.getMostRecentInsideTemp());
+       return M.temperatuur(rawMeasurement.getInsideTemp());
    }
-    
+   
    public double LuchtvochtigheidInsideData()
    {
-       return M.luchtVochtigheid(W.getMostRecentInsideHum());
+       return rawMeasurement.getInsideHum();
    }
    
    public double TemperatuurOutsideData()
    {
-       return M.temperatuur(W.getMostRecentOutsideTemp());
+       return M.temperatuur(rawMeasurement.getOutsideTemp());
    }
    
    public double WindSpeedData()
    {
-       return M.windSnelheid(W.getMostRecentWindSpeed());
+       return M.windSnelheid(rawMeasurement.getWindSpeed());
    }
    
    public double WindSpeedAvData()
    {
-       return M.windSnelheid(W.getMostRecentAvgWindSpeed());       
+       return M.windSnelheid(rawMeasurement.getAvgWindSpeed());       
    }
    
    public String WindRichtingData()
    {
-       return M.windRichting(W.getMostRecentWindDir());
+       return M.windRichting(rawMeasurement.getWindDir());
    }
    
    public double LuchtvochtigheidOutsideData()
    {
-       return M.luchtVochtigheid(W.getMostRecentOutsideHum());
+       return rawMeasurement.getOutsideHum();
    }
    
    public double RainRateData()
    {
-       return M.regenmeter(W.getMostRecentRainRate());
+       return M.regenmeter(rawMeasurement.getRainRate());
    }
    
    public double UVIndexData()
    {
-       return M.uvIndex(W.getMostRecentUVLevel());
+       return M.uvIndex(rawMeasurement.getUVLevel());
+   }
+   
+   public short SolarRad()
+   {
+       return rawMeasurement.getSolarRad();
    }
    
    public double BatteryData()
    {
-       return M.batterySpanning(W.getMostRecentBattLevel());
+       return M.batterySpanning(rawMeasurement.getBattLevel());
    }
    
    public String SunRiseData()
    {
-       return M.sunRise(W.getMostRecentSunrise());
+       return M.sunRise(rawMeasurement.getSunrise());
    }
    
    public String SunSetData()
    {
-       return M.sunSet(W.getMostRecentSunset());
+       return M.sunSet(rawMeasurement.getSunset());
    }
    
+   public String heatIndex()
+    {
+        final double C1 = -42.379,
+                     C2 = 2.04901523,
+                     C3 = 10.14333127,
+                     C4 = -0.22475541,
+                     C5 = -0.00683783,
+                     C6 = -5.481717E-2,
+                     C7 = 1.22874E-3,
+                     C8 = 8.5282E-4,
+                     C9 = -1.99E-6;
+        double T = rawMeasurement.getOutsideTemp()/10;
+        double R = LuchtvochtigheidOutsideData();
+        double T2 = T * T;
+        double R2 = R * R;
+        String answer;
+        double HI;
+        if(TemperatuurOutsideData()>26)
+        {
+            HI = Math.round(((C1 + (C2 * T) + (C3 * R) + (C4 * T * R) + (C5 * T2) + (C6 * R2) + (C7 * T2 * R) + (C8 * T * R2) + (C9 * T2 * R2))-32)/1.8);
+            answer = String.valueOf(HI+"°C");
+        }
+        else
+        {
+            answer = "De temperatuur is te laag om de heat index te berekenen.";
+        }
+        return answer;
+    }
    
+   public String windChill()
+   {
+       double T = TemperatuurOutsideData();
+       double V = WindSpeedData();
+       String answer;
+       double WC;
+       if(T<10 && V>1.3)
+       {
+           WC = 13.12 + (0.6215*T) - (11.37*(Math.pow(V,0.16))+(0.3965*(T*Math.pow(V,0.16))));
+           answer = String.valueOf(WC+"°C");
+       }
+       else if(T>=10 && V<=1.3)
+       {
+           answer = "De temperatuur is te hoog en de windsnelheid is te laag.";
+       }
+       else if(T>=10) 
+       {
+           answer = "De Temperatuur is te hoog.";
+       }
+       else
+       {
+           answer = "De windsnelheid is te laag.";
+       }
+       return answer; 
+   }
    
-
+   public String dewPoint()
+   {
+       double T = TemperatuurOutsideData();
+       double R = LuchtvochtigheidOutsideData();
+       String answer;
+       double D;
+       double a = 17.27;
+       double b = 237.7;
+       double TRL = ((a*T)/(b+T))+ Math.log(R/100);
+       if(0<T && T<100 && 1<R && R<100)
+       {
+           D = Math.round((b*TRL)/(a-TRL));
+           answer = String.valueOf(D+"°C");
+       }
+       else
+       {
+           answer = "Het dauwpunt kan niet worden berekend.";
+       }
+       return answer;
+   }
    
    public void PrintAlles()
    {
        System.out.println(
-       "Datum: "+
+       "Datum en tijd: "+Time()+
        "\nBarometer: "+BarometerData()+"kPa"+
-       "\nBinnentemperatuur: "+TemperatuurInsideData()+"C"+
-       "\nBuitentemperatuur: "+TemperatuurOutsideData()+"C"+
+       "\nBinnentemperatuur: "+TemperatuurInsideData()+"°C"+
+       "\nBuitentemperatuur: "+TemperatuurOutsideData()+"°C"+
        "\nLuchtvochtigheid binnen: "+LuchtvochtigheidInsideData()+"%"+
        "\nLuchtvochtigheid buiten: "+LuchtvochtigheidOutsideData()+"%"+
        "\nWindsnelheid: "+WindSpeedData()+" Km/U"+
        "\nWindrichting: "+WindRichtingData()+
-       "\nGevoelstemperatuur: "+
+       "\nWindchill: "+windChill()+
        "\nBatteryspanning: "+BatteryData()+ " Volt"+
        "\nUV index: "+ UVIndexData()+
-       "\nZonneStraling: "+
-       "\nHeat index: "+
+       "\nZonneStraling: "+SolarRad()+" W/m²"+
+       "\nHeat index: "+heatIndex()+
+       "\nDauwpunt: "+dewPoint()+
        "\nZonsopgang: "+ SunRiseData()+
        "\nZonsondergang: "+ SunSetData()
        
