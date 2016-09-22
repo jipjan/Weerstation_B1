a1480f1c6877a40b787eaa5b469a4cbcd5106b7c
Jacco Steegman <Jacc.stee@gmail.com>
2016-09-22 09:16:48 +0200

    Updated metingenHandler removed bugs and added a new function to clean up sunRise and sunSet

--- awstest/MetingenHandler.java
+++ awstest/MetingenHandler.java
@@ -1,114 +1,135 @@
 
 
 /**
- * Deze class bevat alle methoden voor het omzetten van meetwaarden in Nederlandse eenheden.
+ * This class contains all methods needed to convert the raw mesurements to dutch units
  * @author Groep B1
- * @version 1.0
+ * @version 1.2
  */
 public class MetingenHandler
 {
-    /**
-    * Luchtdruk
-    *
-    * @param mval   Meetwaarde van het vp2pro weerstation
-    * @return De luchtdruk in mbar
-    */
-    public static double luchtdruk(short mval) {
-        return (mval/100d) * 3.3864d;
-    }
+	/**
+	* Luchtdruk
+	*
+	* @param mval   Meetwaarde van het vp2pro weerstation
+	* @return De luchtdruk in mbar
+	*/
+	public static double luchtdruk(short mval)
+	{
+		return Math.round( (mval/100d) * 3.3864d);
+	}
 
-    /**
-    * Temperatuur
-    *
-    * @param mval   Meetwaarde van het vp2pro weerstation
-    * @return De temperatuur in graden Celcius
-    */
-    public static double temperatuur(short mval) {
-        return ((mval / 10d) - 32)/1.8d;
-    }
+	/**
+	* Temperatuur
+	*
+	* @param mval   Meetwaarde van het vp2pro weerstation
+	* @return De temperatuur in graden Celcius
+	*/
+	public static double temperatuur(short mval)
+	{
+		return Math.round( ((mval / 10d) - 32) /1.8d );
+	}
 
-    /**
-    * Relatieve Luchtvochtigheid
-    *
-    * @param mval   Meetwaarde van het vp2pro weerstation
-    * @return De relatieve luchtvochtigheid in procenten
-    */
-    public static double luchtVochtigheid(short mval) {
-        return mval;
-    }
+	/**
+	* Relatieve Luchtvochtigheid
+	*
+	* @param mval   Meetwaarde van het vp2pro weerstation
+	* @return De relatieve luchtvochtigheid in procenten
+	*/
+	public static double luchtVochtigheid(short mval)
+	{
+		return mval;
+	}
 
-    /**
-    * Windsnelheid
-    *
-    * @param mval   Meetwaarde van het vp2pro weerstation
-    * @return De windsnelheid in km/h
-    */
-    public static double windSnelheid(short mval) {
-        return (mval * 1.609344d);
-    }
+	/**
+	* Windsnelheid
+	*
+	* @param mval   Meetwaarde van het vp2pro weerstation
+	* @return De windsnelheid in km/h
+	*/
+	public static double windSnelheid(short mval)
+	{
+		return Math.round( (mval * 1.609344d));
+	}
 
-    /**
-    * Windrichting
-    *
-    * @param mval   Meetwaarde van het vp2pro weerstation
-    * @return De windrichting In windroos richtingen
-    */
-    public static String windRichting(short mval) {
-        String mogelijkWindrichting[] = {"N", "NO", "O", "ZO", "Z", "ZW", "W", "NW", "N"};
-        return mogelijkWindrichting[mval/45];
-    }
+	/**
+	* Windrichting
+	*
+	* @param mval   Meetwaarde van het vp2pro weerstation
+	* @return De windrichting In windroos richtingen
+	*/
+	public static String windRichting(short mval)
+	{
+		String mogelijkWindrichting[] = {"N", "NO", "O", "ZO", "Z", "ZW", "W", "NW", "N"};
+		return mogelijkWindrichting[mval /45];
+	}
 
-    /**
-    * Regenmeter
-    *
-    * @param mval   Meetwaarde van het vp2pro weerstation
-    * @return De hoeveelheid regen in mm
-    */
-    public static double regenmeter(short mval) {
-        return (mval*0.2);
-    }
+	/**
+	* Regenmeter
+	*
+	* @param mval   Meetwaarde van het vp2pro weerstation
+	* @return De hoeveelheid regen in mm
+	*/
+	public static double regenmeter(short mval)
+	{
+		return Math.round((mval *0.2));
+	}
 
-    /**
-    * uvIndex
-    *
-    * @param mval   Meetwaarde van het vp2pro weerstation
-    * @return De windrichting in graden
-    */
-    public static double uvIndex(short mval) {
-        return (mval/10);
-    }
+	/**
+	* uvIndex
+	*
+	* @param mval   Meetwaarde van het vp2pro weerstation
+	* @return De windrichting in graden
+	*/
+	public static double uvIndex(short mval)
+	{
+		return Math.round((mval /10));
+	}
 
-    /**
-    * batterySpanning
-    *
-    * @param mval   Meetwaarde van het vp2pro weerstation
-    * @return De battery spanning in Volt
-    */
-    public static double batterySpanning(short mval) {
-        return (((mval*300)/512d)/100d);
-    }
+	/**
+	* batterySpanning
+	*
+	* @param mval   Meetwaarde van het vp2pro weerstation
+	* @return De battery spanning in Volt
+	*/
+	public static double batterySpanning(short mval)
+	{
+		return Math.round(((mval *300) /512d) /100d);
+	}
 
-    /**
-    * sunRise
-    *
-    * @param mval	Meetwaarde van het vp2pro weerstation
-    * @return Zonsopkomst in hh:mm notatie
-    */
-    public static String sunRise(short mval) {
-        String toReturn = Short.toString(mval);
-        if (toReturn.length() == 3)
-            return "0" + toReturn.substring(0,1) + ":" + toReturn.substring(1, 3);
-        else
-            return toReturn.substring(0,2) + ":" + toReturn.substring(2, 4);
-    }
+	/**
+	 * TimeShortToString
+	 *
+	 * @param shortTime 	de tijd gegeven in een short
+	 * @return de tijd gegeven in een string in hh:mm notatie
+	 */
+	private static String TimeShortToString(short shortTime)
+	{
+		String toReturn = Short.toString(shortTime);
+		if (toReturn.length() == 3)
+			return "0" + toReturn.substring(0,1) + ":" + toReturn.substring(1, 3);
+		else
+			return toReturn.substring(0,2) + ":" + toReturn.substring(2, 4);
+	}
 
-    /**
-    * sunSet
-    *
-    * @param mval	Meetwaarde van het vp2pro weerstation
-    * @return Zonsondergang in hh:mm notatie
-    */
-   public static String sunSet(short mval) {
-       return sunRise(mval);
-    }
+	/**
+	* sunRise
+	*
+	* @param mval	Meetwaarde van het vp2pro weerstation
+	* @return Zonsopkomst in hh:mm notatie
+	*/
+	public static String sunRise(short mval)
+	{
+		return TimeShortToString(mval);
+	}
+
+	/**
+	* sunSet
+	*
+	* @param mval	Meetwaarde van het vp2pro weerstation
+	* @return Zonsondergang in hh:mm notatie
+	*/
+	public static String sunSet(short mval)
+	{
+	   return TimeShortToString(mval);
+	}
 }
